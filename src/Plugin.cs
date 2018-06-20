fcf1f0d24be0c2c62369ad8667b04817488b5025
David Pires <david.ferreira.pires@everis.com>
2018-06-20 18:54:36 +0100

    Added basic plugin structure

---
+++ src/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Subble.Core;
+using Subble.Core.Plugin;
+using System.Collections.Generic;
+using Subble.Core.Config;
+
+namespace AccessManager
+{
+    public class Plugin : ISubblePlugin
+    {
+        public IPluginInfo Info
+            => new PluginInfo();
+
+        public SemVersion Version
+            => new SemVersion(0, 1, 0);
+
+        public long LoadPriority => 5;
+
+        public IEnumerable<Dependency> Dependencies
+            => new List<Dependency> {
+                new Dependency(typeof(IConfigManager), (0,1,0))
+            };
+
+        public bool Initialize(ISubbleHost host)
+        {
+            if (host is null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/PluginInfo.cs
@@ -0,0 +1,19 @@
+﻿using Subble.Core.Plugin;
+
+namespace AccessManager
+{
+    public class PluginInfo : IPluginInfo
+    {
+        public string GUID => "b0391e65-3506-4713-bdf6-2c2bd53d538a";
+
+        public string Name => "AccessManager";
+
+        public string Creator => "DavidPires";
+
+        public string Repository => "https://github.com/Davidblkx/Subble";
+
+        public string Support => "https://github.com/Davidblkx/Subble/issues";
+
+        public string Licence => "MIT";
+    }
+}
